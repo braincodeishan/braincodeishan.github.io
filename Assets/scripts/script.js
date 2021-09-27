0acb9eccc31dea9979df8410ac223b132b5be62e
braincodeishan <braincodeishan@gmail.com>
2021-09-27 13:06:29 +0530

    27.09.2021 001

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -6,8 +6,66 @@ dashboardboxs.forEach(dashboardbox => {
     })
 })
 
-    function removeActive() {
-        dashboardboxs.forEach(dashboardbox => {
-            dashboardbox.classList.remove('selected');
-        })
+function removeActive() {
+    dashboardboxs.forEach(dashboardbox => {
+        dashboardbox.classList.remove('selected');
+    })
+}
+
+
+const posb_overall = document.getElementById('posb_overall');
+const posb_scheme = document.getElementById('posb_scheme');
+
+function removePosbCategoryActive() {
+    posb_overall.classList.remove('active');
+    posb_scheme.classList.remove('active');
+    const buttonposb = document.getElementsByClassName('buttonposb');
+    var id = "";
+    for (i = 0; i < buttonposb.length; i++) {
+        if (buttonposb[i].classList.contains('active')) {
+            id = buttonposb[i].id;
+            break;
+        }
     }
+
+    return id;
+}
+posb_overall.addEventListener('click', () => {
+
+
+    var id = removePosbCategoryActive();
+    posb_overall.classList.add('active');
+    activeData(id);
+})
+posb_scheme.addEventListener('click', () => {
+
+    var id = removePosbCategoryActive();
+    posb_scheme.classList.add('active');
+    activeData(id);
+})
+
+function activeData(id) {
+    const findid = document.getElementsByClassName(id);
+    const buttonid = document.getElementById(id);
+    const posb = document.getElementsByClassName('posb');
+    const buttonposb = document.getElementsByClassName('buttonposb');
+    for (i = 0; i < posb.length; i++) {
+        posb[i].classList.remove('active');
+    }
+    for (i = 0; i < buttonposb.length; i++) {
+        buttonposb[i].classList.remove('active');
+    }
+   
+    if (posb_overall.classList.contains('active')) {
+        findid[0].classList.add('active');
+
+    } else {
+        findid[1].classList.add('active');
+    }
+    buttonid.classList.add('active')
+    
+}
+
+
+
+
